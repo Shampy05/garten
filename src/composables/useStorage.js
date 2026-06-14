7c91f225b660992c739e3e479cdd42390326a015
Shampy05 <sharmapranshu317@gmail.com>
2026-06-14 19:21:27 +0100

    fix: replace language object on color update to trigger reactivity

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -146,7 +146,7 @@ export function useStorage() {
     }
     clearCache(userId.value)
     const idx = data.value.languages.findIndex(l => l.id === langId)
-    if (idx !== -1) Object.assign(data.value.languages[idx], updates)
+    if (idx !== -1) data.value.languages[idx] = { ...data.value.languages[idx], ...updates }
   }
 
   const deleteEntry = async (id) => {
